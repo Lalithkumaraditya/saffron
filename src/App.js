0fb7fe6eb9127344a8d1344196c089497194ed55
Lalithkumaraditya <lalithkumaraditya@gmail.com>
2020-02-15 16:46:52 +0530

    google signin is working

--- src/App.js
+++ src/App.js
@@ -1,59 +1,48 @@
-import React from "react";
-import HomePage from './components/home-page/home-page.component';
-import ShopPage from '../src/components/pages/shop/shop.component';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import "./App.css";
-import {Switch,Route} from 'react-router-dom';
-import Header from "./components/header/header.component";
-import SignInAndSignUpPage from "./components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import {auth} from './firebase/firebase.utils';
+import './App.css';
 
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
+class App extends React.Component {
+  constructor() {
+    super();
 
-const HatsPage = ()=>(
-  <div>
-  <h1> Hats Page dude...</h1>
-  </div>
-)
-
-class  App extends React.Component {
-
-  constructor(props) {
-    super(props)
-  
     this.state = {
-       currentUser:null
-    }
+      currentUser: null
+    };
   }
-  
-  unsubscribeFromAuth=null;
-  
+
+  unsubscribeFromAuth = null;
+
   componentDidMount() {
-   this.unsubscribeFromAuth= auth.onAuthStateChanged(user =>{this.setState({currentUser:user});
-  console.log(user);
-  });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
   }
-  componentWillUnmount(){
+
+  componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
-  
 
-  render(){
-
-    return(
+  render() {
+    return (
       <div>
-      <Header />
-      <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route  path="/shop" component={ShopPage} />
-      <Route  path="/sigin" component={SignInAndSignUpPage} />
-      </Switch>
-      
-  
-    
-  
-    </div>
-    )
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
   }
 }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,21 +3,23 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config ={
-    apiKey: "AIzaSyDF9iePeZn-2SpFAo4lkrJbPILAVXJNf6Y",
-    authDomain: "saffron-db.firebaseapp.com",
-    databaseURL: "https://saffron-db.firebaseio.com",
-    projectId: "saffron-db",
-    storageBucket: "saffron-db.appspot.com",
-    messagingSenderId: "514831307224",
-    appId: "1:514831307224:web:99e7548c0c25ca01c1e2ad",
-    measurementId: "G-7CH0N72KDM"
-  };
+  apiKey: "AIzaSyDF9iePeZn-2SpFAo4lkrJbPILAVXJNf6Y",
+  authDomain: "saffron-db.firebaseapp.com",
+  databaseURL: "https://saffron-db.firebaseio.com",
+  projectId: "saffron-db",
+  storageBucket: "saffron-db.appspot.com",
+  messagingSenderId: "514831307224",
+  appId: "1:514831307224:web:99e7548c0c25ca01c1e2ad",
+  measurementId: "G-7CH0N72KDM"
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  export const auth =firebase.auth();
-  export  const firestore=firebase.firestore();
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({prompt:'select_account'});
-  export const signInWithGoogle= () => auth.signInWithPopup(provider);
-  export default firebase;
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
